141da80808ebd9c1a1ee2bdad129da26cde8028e
Elena Makarova <el-makarova@yandex-team.ru>
2025-04-03 13:34:56 +0300

    fix: ThemeProvider should wrap AsideHeader too

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,22 +1,31 @@
 'use client';
 
 import React from 'react';
+import {Theme, ThemeProvider} from '@gravity-ui/uikit';
 
 import {Ghost} from '@gravity-ui/icons';
 import {AsideHeader} from '@gravity-ui/navigation';
 
-import {Wrapper} from '../Wrapper';
+import {DARK, DEFAULT_THEME, Wrapper} from '../Wrapper';
 
 interface AppProps {
     children: React.ReactNode;
 }
 export const App: React.FC<AppProps> = ({children}) => {
+    const [theme, setTheme] = React.useState<Theme>(DEFAULT_THEME);
+    const isDark = theme === DARK;
     return (
-        <AsideHeader
-            logo={{icon: Ghost, text: 'nextjs-example'}}
-            compact={true}
-            hideCollapseButton={true}
-            renderContent={() => <Wrapper>{children}</Wrapper>}
-        />
+        <ThemeProvider theme={theme}>
+            <AsideHeader
+                logo={{icon: Ghost, text: 'nextjs-example'}}
+                compact={true}
+                hideCollapseButton={true}
+                renderContent={() => (
+                    <Wrapper setTheme={setTheme} isDark={isDark}>
+                        {children}
+                    </Wrapper>
+                )}
+            />
+        </ThemeProvider>
     );
 };
